9bf5020e60428f58f2e022ae301124d798a6c8f6
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-03-23 11:10:13 +0100

    Show and export all trackmate data.

--- CHANGES.md
+++ CHANGES.md
@@ -2,8 +2,17 @@
 
 * Contacts that are less than the minimal number of frames for stable
   contact, but satisfy the minimal overlap for contact are now counted
-  under the 'Transient contacts' in the data table.
+  under the 'Transient contacts' in the data table. They do not 
+  affect the Target visited metric.
 
+* Show all per cell (track) trackmate data in the user interface
+  datatable in and in the CSV export. In the user interface column
+  labels links to their definition in the trackmate documentation if
+  you click them.
+
+  The column order can't be changed in the ui/csv but I can easily
+  tweak it in the code if you tell me what you prefer to see first.
+  
 * Add a T dead limit: a slider to determine a "minimal maximal distance 
   travelled" to take a T cell into consideration.
   
--- src_gui/datatable.ml
+++ src_gui/datatable.ml
@@ -7,303 +7,261 @@ open Brr
 
 (* Data encoders into table data and csv *)
 
-type 'a enc = { td : 'a -> El.t; csv : Buffer.t -> 'a -> unit }
+type 'a enc = { csv : Buffer.t -> 'a -> unit; td : 'a -> El.t }
+
+let enc_td v = El.td ~at:At.[Negsp.Text.align `End] [El.txt v]
 
 let int_enc =
-  let td i = El.td ~at:At.[Negsp.Text.align `End] [El.txt (Jstr.of_int i)] in
-  let csv b i =
-    Buffer.add_string b (string_of_int i); Buffer.add_char b ','
-  in
-  { td; csv }
+  let csv b i = Buffer.add_string b (string_of_int i) in
+  let td i = enc_td (Jstr.of_int i) in
+  { csv; td }
 
 let int_opt_enc =
-  let td i =
-    let i = match i with None -> [] | Some i -> [El.txt (Jstr.of_int i)] in
-    El.td ~at:At.[Negsp.Text.align `End] i
+  let csv b = function
+  | None -> () | Some i -> Buffer.add_string b (Int.to_string i)
   in
-  let csv b i =
-    let i = match i with None -> "" | Some i -> Int.to_string i in
-    Buffer.add_string b i; Buffer.add_char b ','
+  let td = function
+  | None -> enc_td Jstr.empty | Some i -> enc_td (Jstr.of_int i)
   in
-  { td; csv }
+  { csv; td }
 
 let float_enc_with ~frac () =
-  let td f =
-    El.td ~at:At.[Negsp.Text.align `End] [El.txt (Jstr.of_float ~frac f)]
-  in
-  let csv b f =
-    Buffer.add_string b (string_of_float f); Buffer.add_char b ','
-  in
-  { td; csv }
+  let csv b f = Buffer.add_string b (string_of_float f) in
+  let td f = enc_td (Jstr.of_float ~frac f) in
+  { csv; td }
 
 let float_enc = float_enc_with ~frac:2 ()
 
+let time_enc = (* We show it with another unit in the ui *)
+  let to_s = 1. /. 60. in
+  let csv b f = Buffer.add_string b (string_of_float f) in
+  let td f = enc_td (Jstr.of_float ~frac:2 (f *. to_s)) in
+  { csv; td }
+
 type 'a col =
   { name : string;
+    name_th : string; (* If "" [name] is used *)
     href : string option;
     enc : 'a enc;
-    get : Cell.t -> Trackmate.track -> 'a }
+    get : Cell.t -> Trackmate.track -> Cell.Contact.t list option -> 'a }
 
 type ecol = C : 'a col -> ecol
 
 let href h =
   Some ("https://imagej.net/imagej-wiki-static/TrackMate_Algorithms.html#" ^ h)
 
-(*
-  "Id";
-  "Contacts";
-  "Targets visited";
-   *)
-
 (* Track branching analyser *)
 
 let number_spots =
-  { name = "N spots"; href = href "Number_of_spots_in_track.";
-    enc = int_enc; get = (fun _ t -> t.number_spots) }
+  { name = "N spots"; name_th = ""; href = href "Number_of_spots_in_track.";
+    enc = int_enc; get = (fun _ t _ -> t.number_spots) }
 
 let number_gaps =
-  { name = "N gaps"; href = href "Number_of_gaps.";
-    enc = int_enc; get = (fun _ t -> t.number_gaps); }
+  { name = "N gaps"; name_th = ""; href = href "Number_of_gaps.";
+    enc = int_enc; get = (fun _ t _ -> t.number_gaps); }
 
 let longest_gap =
-  { name = "Lgst gap"; href = href "Longest_gap.";
-    enc = int_enc; get = (fun _ t -> t.longest_gap); }
+  { name = "Lgst gap"; name_th = ""; href = href "Longest_gap.";
+    enc = int_enc; get = (fun _ t _ -> t.longest_gap); }
 
 let number_splits =
-  { name = "N splits"; href = href "Number_of_split_events.";
-    enc = int_enc; get = (fun _ t -> t.number_splits); }
+  { name = "N splits"; name_th = ""; href = href "Number_of_split_events.";
+    enc = int_enc; get = (fun _ t _ -> t.number_splits); }
 
 let number_merges =
-  { name = "N merges"; href = href "Number_of_merge_event.";
-    enc = int_enc; get = (fun _ t -> t.number_merges); }
+  { name = "N merges"; name_th = ""; href = href "Number_of_merge_event.";
+    enc = int_enc; get = (fun _ t _  -> t.number_merges); }
 
 let number_complex =
-  { name = "N complex"; href = href "Complex_points.";
-    enc = int_enc; get = (fun _ t -> t.number_complex); }
+  { name = "N complex"; name_th = ""; href = href "Complex_points.";
+    enc = int_enc; get = (fun _ t _ -> t.number_complex); }
 
 (* Track duration *)
 
 let track_duration =
-  { name = "Duration"; href = href "Duration_of_track.";
-    enc = float_enc; get = (fun _ t -> t.track_duration); }
+  { name = "Duration"; name_th = "Duration (s)";
+    href = href "Duration_of_track.";
+    enc = time_enc; get = (fun _ t _ -> t.track_duration); }
 
 let track_start =
-  { name = "Start"; href = href "Track_start.";
-    enc = float_enc; get = (fun _ t -> t.track_start); }
+  { name = "Start"; name_th = "Start (s)"; href = href "Track_start.";
+    enc = time_enc; get = (fun _ t _ -> t.track_start); }
 
 let track_stop =
-  { name = "Stop"; href = href "Track_stop.";
-    enc = float_enc; get = (fun _ t -> t.track_stop); }
+  { name = "Stop"; name_th = "Stop (s)"; href = href "Track_stop.";
+    enc = time_enc; get = (fun _ t _ -> t.track_stop); }
 
 let track_displacement =
-  { name = "Displacement"; href = href "Track_displacement.";
-    enc = float_enc; get = (fun _ t -> t.track_displacement) }
+  { name = "Displacement"; name_th = ""; href = href "Track_displacement.";
+    enc = float_enc; get = (fun _ t _ -> t.track_displacement) }
 
 (* Track location *)
 
 let track_x_location =
-  { name = "X"; href = href "X_Location_.28mean.29.";
-    enc = float_enc; get = (fun _ t -> t.track_x_location); }
+  { name = "X"; name_th = ""; href = href "X_Location_.28mean.29.";
+    enc = float_enc; get = (fun _ t _ -> t.track_x_location); }
 
 let track_y_location =
-  { name = "Y"; href = href "Y_Location_.28mean.29.";
-    enc = float_enc; get = (fun _ t -> t.track_y_location); }
+  { name = "Y"; name_th = ""; href = href "Y_Location_.28mean.29.";
+    enc = float_enc; get = (fun _ t _ -> t.track_y_location); }
 
 let track_z_location =
-  { name = "Z"; href = href "Z_Location_.28mean.29.";
-    enc = float_enc; get = (fun _ t -> t.track_z_location); }
+  { name = "Z"; name_th = ""; href = href "Z_Location_.28mean.29.";
+    enc = float_enc; get = (fun _ t _ -> t.track_z_location); }
 
 (* Track speed *)
 
 let track_mean_speed =
-  { name = "Mean sp."; href = href "Mean_velocity.";
-    enc = float_enc; get = (fun _ t -> t.track_mean_speed) }
+  { name = "Mean sp."; name_th = ""; href = href "Mean_velocity.";
+    enc = float_enc; get = (fun _ t _ -> t.track_mean_speed) }
 
 let speed_derived =
   "Maximal_velocity.2C_Minimal_velocity.\
    2C_Median_velocity_and_Velocity_standard_deviation."
 
 let track_max_speed =
-  { name = "Max sp."; href = href speed_derived;
-    enc = float_enc; get = (fun _ t -> t.track_max_speed) }
+  { name = "Max sp."; name_th = ""; href = href speed_derived;
+    enc = float_enc; get = (fun _ t _ -> t.track_max_speed) }
 
 let track_min_speed =
-  { name = "Min sp."; href = href speed_derived;
-    enc = float_enc; get = (fun _ t -> t.track_min_speed) }
+  { name = "Min sp."; name_th = ""; href = href speed_derived;
+    enc = float_enc; get = (fun _ t _ -> t.track_min_speed) }
 
 let track_median_speed =
-  { name = "Med. sp."; href = href speed_derived;
-    enc = float_enc; get = (fun _ t -> t.track_median_speed) }
+  { name = "Med. sp."; name_th = ""; href = href speed_derived;
+    enc = float_enc; get = (fun _ t _ -> t.track_median_speed) }
 
 let track_std_speed =
-  { name = "Std sp."; href = href speed_derived;
-    enc = float_enc; get = (fun _ t -> t.track_std_speed) }
+  { name = "Std sp."; name_th = ""; href = href speed_derived;
+    enc = float_enc; get = (fun _ t _ -> t.track_std_speed) }
 
 (* Spot quality *)
 
 let track_mean_quality =
-  { name = "Mean Q"; href = href "Track_spot_quality.";
-    enc = float_enc; get = (fun _ t -> t.track_mean_quality) }
+  { name = "Mean Q"; name_th = ""; href = href "Track_spot_quality.";
+    enc = float_enc; get = (fun _ t _ -> t.track_mean_quality) }
 
 (* Linear track analysis *)
 
 let total_distance_traveled =
-  { name = "Total dist."; href = href "Total_distance_traveled.";
-    enc = float_enc; get = (fun _ t -> t.total_distance_traveled); }
+  { name = "Total dist."; name_th = ""; href = href "Total_distance_traveled.";
+    enc = float_enc; get = (fun _ t _ -> t.total_distance_traveled); }
 
 let max_distance_traveled =
-  { name = "Max dist."; href = href "Max_distance_traveled.";
-    enc = float_enc; get = (fun _ t -> t.max_distance_traveled); }
+  { name = "Max dist."; name_th = ""; href = href "Max_distance_traveled.";
+    enc = float_enc; get = (fun _ t _ -> t.max_distance_traveled); }
 
 let confinement_ratio =
-  { name = "Cnf. ratio"; href = href "Confinement_ratio.";
-    enc = float_enc; get = (fun _ t -> t.confinement_ratio); }
+  { name = "Cnf. ratio"; name_th = ""; href = href "Confinement_ratio.";
+    enc = float_enc; get = (fun _ t _ -> t.confinement_ratio); }
 
 let mean_straigth_line_speed =
-  { name = "Mean line sp."; href = href "Mean_straight_line_speed.";
-    enc = float_enc; get = (fun _ t -> t.mean_straight_line_speed); }
+  { name = "Mean line sp."; name_th = "";
+    href = href "Mean_straight_line_speed.";
+    enc = float_enc; get = (fun _ t _ -> t.mean_straight_line_speed); }
 
 let linearity_of_forward_progression =
   { name = "Fwd. progr.";
-    href = href "Linearity_of_forward_progression.";
-    enc = float_enc; get = (fun _ t -> t.linearity_of_forward_progression); }
+    name_th = ""; href = href "Linearity_of_forward_progression.";
+    enc = float_enc; get = (fun _ t _ -> t.linearity_of_forward_progression); }
 
 let mean_directional_change_rate =
   { name = "Mn. y rate";
-    href = href "Mean_directional_change.";
-    enc = float_enc; get = (fun _ t -> t.mean_directional_change_rate); }
+    name_th = ""; href = href "Mean_directional_change.";
+    enc = float_enc; get = (fun _ t _ -> t.mean_directional_change_rate); }
+
+let id =
+  { name = "Id";
+    name_th = ""; href = href "Track_ID.";
+    enc = int_enc; get = (fun _ t _ -> t.tid) }
+
+let contacts =
+  { name = "Contacts"; name_th = ""; href = None;
+    enc = int_opt_enc;
+    get = (fun _ _ c -> Option.map Cell.Contact.count c); }
+
+let targets_visited =
+  { name = "Tgt visited"; name_th = ""; href = None;
+    enc = int_opt_enc;
+    get = (fun _ _ c -> Option.map Cell.Contact.unique_count c) }
 
 let cols =
-  [ (* Track branching *)
-    C number_spots; C number_gaps; C longest_gap;
-    C number_splits; C number_merges; C number_complex;
-    (* Track duration *)
-    C track_duration; C track_start; C track_stop; C track_displacement;
-    (* Track location *)
-    C track_x_location; C track_y_location; C track_z_location;
+  [ C id;
+    C contacts;
+    C targets_visited;
+    C track_start; C track_stop;
+    C max_distance_traveled;
+    (* Linear track analysis *)
+    C total_distance_traveled; C confinement_ratio;
+    C mean_straigth_line_speed; C linearity_of_forward_progression;
     (* Track velocity *)
     C track_mean_speed; C track_max_speed; C track_min_speed;
     C track_median_speed; C track_std_speed;
+    (* Track duration *)
+    C track_duration; C track_displacement;
+    (* Track branching *)
+    C number_spots; C number_gaps; C longest_gap;
+    C number_splits; C number_merges; C number_complex;
+    (* Track location *)
+    C track_x_location; C track_y_location; C track_z_location;
     (* Spot quality *)
     C track_mean_quality;
-    (* Linear track analysis *)
-    C total_distance_traveled; C max_distance_traveled; C confinement_ratio;
-    C mean_straigth_line_speed; C linearity_of_forward_progression;
-  ]
-
-(*
-  "Mean speed";
-  "Median speed";
-  "Max speed";
-  "Min speed";
-  "Max dist.";
-  "Total dist.";
-  "Start time";
-  "Stop time";
-  "Splits";
-  "Merges";
-  "Gaps";
-*)
-
-
-let headers =
-[
-  "Id";
-  "Contacts";
-  "Targets visited";
-  "Mean speed";
-  "Median speed";
-  "Max speed";
-  "Min speed";
-  "Max dist.";
-  "Total dist.";
-  "Start time";
-  "Stop time";
-  "Splits";
-  "Merges";
-  "Gaps"; ]
-
+ ]
 
 let to_csv tm targets contacts =
   let b = Buffer.create 5000 in
-  let int = int_enc.csv b in
-  let int_option = int_opt_enc.csv b in
-  let float = float_enc.csv b in
-  let add_header b h =
-    Buffer.add_string b (Printf.sprintf {|"%s"|} h); Buffer.add_char b ','
+  let rec add_headers b = function
+  | [] -> Buffer.add_string b "\r\n"
+  | (C c) :: cs ->
+      Buffer.add_string b (Printf.sprintf {|"%s"|} c.name);
+      if cs <> [] then Buffer.add_char b ',';
+      add_headers b cs
   in
-  List.iter (add_header b) headers;
-  Buffer.add_char b '\n';
+  let rec add_row b cell track contacts = function
+  | [] -> Buffer.add_string b "\r\n"
+  | (C c) :: cs ->
+      let v = c.get cell track contacts in
+      c.enc.csv b v; if cs <> [] then Buffer.add_char b ',';
+      add_row b cell track contacts cs
+  in
+  add_headers b cols;
   for i = 0 to Array.length targets - 1 do
     let cell = targets.(i) in
-    let unique =
-      Option.map (fun c -> Cell.Contact.unique_count c.(i)) contacts
-    in
-    let contacts =
-      Option.map (fun c -> Cell.Contact.count c.(i)) contacts
-    in
-    let t =
+    let track =
+      Option.get @@
       Trackmate.Int_map.find_opt cell.Cell.track_id tm.Trackmate.tracks_by_id
     in
-    let t = Option.get t in
-    int t.Trackmate.tid;
-    int_option contacts;
-    int_option unique;
-    float t.Trackmate.track_mean_speed;
-    float t.Trackmate.track_median_speed;
-    float t.Trackmate.track_max_speed;
-    float t.Trackmate.track_min_speed;
-    float t.Trackmate.max_distance_traveled;
-    float t.Trackmate.total_distance_traveled;
-    float t.Trackmate.track_start;
-    float t.Trackmate.track_stop;
-    int t.Trackmate.number_splits;
-    int t.Trackmate.number_merges;
-    int t.Trackmate.number_gaps;
-    Buffer.add_char b '\n';
+    let contacts = Option.map (fun c -> c.(i)) contacts in
+    add_row b cell track contacts cols
   done;
   Buffer.contents b
 
-let thead g =
-  El.thead ~at:At.[Negsp.Text.align `End] [
-    El.tr [ El.th [El.txt' "Id"];
-            El.th [El.txt' "Contacts"];
-            El.th [El.txt' "Targets visited"];
-            El.th [El.txt' "Median speed"];
-            El.th [El.txt' "Max dist."];
-            El.th [El.txt' "Total dist."];
-            El.th [El.txt' "Start time"];
-            El.th [El.txt' "Stop time"];
-            El.th [El.txt' "Splits"];
-            El.th [El.txt' "Merges"];
-            El.th [El.txt' "Gaps"]]]
+let thead g cols =
+  let th (C c) =
+    let name = if c.name_th <> "" then c.name_th else c.name in
+    match c.href with
+    | None -> El.th [El.txt' name]
+    | Some href_def ->
+        let tgt = At.v (Jstr.v "target") (Jstr.v "_blank") in
+        El.th [El.a ~at:At.[tgt; href (Jstr.v href_def)] [El.txt' name]]
+  in
+  El.thead ~at:At.[Negsp.Text.align `End] [El.tr (List.map th cols)]
+
 
 let selected = Jstr.v "selected"
 
 let tr sel_el sel_i on_click tm g contacts i =
-  let td_int = int_enc.td in
-  let td_int_option = int_opt_enc.td in
-  let td_float = float_enc.td in
   let cell = g.(i) in
-  let unique = Option.map (fun c -> Cell.Contact.unique_count c.(i)) contacts in
-  let contacts = Option.map (fun c -> Cell.Contact.count c.(i)) contacts in
-  let t =
+  let track =
+    Option.get @@
     Trackmate.Int_map.find_opt cell.Cell.track_id tm.Trackmate.tracks_by_id
   in
-  let t = Option.get t in
-  let tr = El.tr [
-      td_int t.Trackmate.tid;
-      td_int_option contacts;
-      td_int_option unique;
-      td_float t.Trackmate.track_median_speed;
-      td_float t.Trackmate.max_distance_traveled;
-      td_float t.Trackmate.total_distance_traveled;
-      td_float (t.Trackmate.track_start /. 60.);
-      td_float (t.Trackmate.track_stop /. 60.);
-      td_int t.Trackmate.number_splits;
-      td_int t.Trackmate.number_merges;
-      td_int t.Trackmate.number_gaps]
+  let contacts = Option.map (fun c -> c.(i)) contacts in
+  let td cell track contacts (C c) =
+    let v = c.get cell track contacts in
+    c.enc.td v
   in
+  let tr = El.tr (List.map (td cell track contacts) cols) in
   ignore (Ev.listen Ev.click (on_click i) (El.as_target tr));
   begin match sel_i with
   | Some j when i = j -> El.set_class selected true tr; sel_el := Some tr
@@ -333,7 +291,7 @@ let of_cell_group tm g ~contacts ~sel =
         El.set_class selected true t;
         sel_el := Some t; set_sel (Some i)
   in
-  let thead = thead g in
+  let thead = thead g cols in
   let tbody =
     El.tbody (List.init (Array.length g)
                 (tr sel_el sel_i on_click tm g contacts))
