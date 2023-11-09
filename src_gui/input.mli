6badf80a9624771e268b4a623c3c00493c7bcb5d
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-11-09 09:28:54 +0100

    Spdxify.

--- src_gui/input.mli
+++ src_gui/input.mli
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2022 The vz programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 open Note
@@ -200,19 +200,3 @@ val files :
       {- [enabled], indicates whether the input can be actuated,
          defaults to {!S.Bool.true'}.}
       {- [label], a label for the input, if any.}} *)
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2022 The vz programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
--- src_gui/output.mli
+++ src_gui/output.mli
@@ -1,6 +1,6 @@
 (*---------------------------------------------------------------------------
    Copyright (c) 2022 The vz programmers. All rights reserved.
-   Distributed under the ISC license, see terms at the end of the file.
+   SPDX-License-Identifier: ISC
   ---------------------------------------------------------------------------*)
 
 open Note
@@ -18,19 +18,3 @@ val image_view' : (Gg.box2 * Vg.image) Lazy.t Note.signal -> Brr.El.t
 val spinner : bool signal -> El.t
 val span : ?at:At.t list -> string signal -> El.t
 val block : ?at:At.t list -> string signal -> El.t
-
-(*---------------------------------------------------------------------------
-   Copyright (c) 2022 The vz programmers
-
-   Permission to use, copy, modify, and/or distribute this software for any
-   purpose with or without fee is hereby granted, provided that the above
-   copyright notice and this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-  ---------------------------------------------------------------------------*)
