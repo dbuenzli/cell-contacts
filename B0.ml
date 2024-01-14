4582e22a4a3e7718d96e853347e093438d30b75b
Daniel Bünzli <daniel.buenzli@erratique.ch>
2024-01-14 18:44:39 +0100

    Track b0.

--- B0.ml
+++ B0.ml
@@ -16,27 +16,13 @@ let brr_poked = B0_ocaml.libname "brr.poked"
 let note = B0_ocaml.libname "note"
 let note_brr = B0_ocaml.libname "note.brr"
 let evidence = B0_ocaml.libname "vz.evidence"
-
 let cmdliner = B0_ocaml.libname "cmdliner"
-
 (* let vz_doc = B0_ocaml.libname "vz.doc" *)
 
-let show_uri_action f build ?env ?cwd u ~args =
-  (* FIXME get that into b0 *)
-  let open Result.Syntax in
-  Fut.return @@ Log.if_error ~use:B0_cli.Exit.some_error @@
-  let url = Fpath.(B0_build.build_dir build u / f) in
-  let background = true and prefix = true and
-    url = Fmt.str "file://%s" (Fpath.to_string url)
-  in
-  let* b = B0_web_browser.find ~browser:None () in
-  let* () = B0_web_browser.show ~background ~prefix b url in
-  Ok B0_cli.Exit.ok
-
 let vcs_describe b =
-  (* XXX memo ? *)
+  (* TODO memo ? *)
   let open Result.Syntax in
-  let dir = B0_build.scope_dir b (B0_build.current b) in
+  let dir = B0_build.scope_dir b in
   let* vcs = B0_vcs_repo.get () ~dir in
   B0_vcs_repo.describe vcs ~dirty_mark:true "HEAD"
 
@@ -46,7 +32,7 @@ let negsp_css = ~/"src/gui/negsp.css"
 let gui_css = ~/"src/gui/gui.css"
 let linker = ~/"src/gui/gui.html"
 
-let data_url b ~type' file = (* XXX add something like that to b0 *)
+let data_url b ~type' file = (* TODO add something like that to b0 *)
   let open Fut.Syntax in
   let* favicon = B0_memo.read (B0_build.memo b) file in
   let b64 = B0_base64.encode favicon in
@@ -61,12 +47,11 @@ let build_cell_gui proc b =
   let gui_css = B0_build.in_scope_dir b gui_css in
   let negsp_css = B0_build.in_scope_dir b negsp_css in
   let linker = B0_build.in_scope_dir b linker in
-  let script = B0_build.in_build_dir b (Fpath.v "gui.js") in
-  let app = B0_build.in_build_dir b (Fpath.v "cell.html") in
+  let script = B0_build.in_current_dir b (Fpath.v "gui.js") in
+  let app = B0_build.in_current_dir b (Fpath.v "cell.html") in
   let reads = [favicon; font; negsp_css; gui_css; linker; script] in
   let version = vcs_describe b |> B0_memo.fail_if_error m in
-  List.iter
-    (B0_memo.file_ready m) [favicon;font; gui_css; negsp_css; linker];
+  B0_memo.ready_files m [favicon; font; gui_css; negsp_css; linker];
   begin B0_memo.write m ~reads ~stamp:version app @@ fun () ->
     Fut.sync @@
     let* linker = B0_memo.read m linker in
@@ -89,18 +74,22 @@ let build_cell_gui proc b =
   end;
   Fut.return ()
 
+(* Units *)
+
+let protocol_md = ~/"PROTOCOL.md"
 let protocol =
+  let html = Fpath.(protocol_md -+ ".html") in
   let exe_proc b =
     let open Fut.Syntax in
     let m = B0_build.memo b in
-    let scope = B0_build.current_scope_dir b in
-    let md = Fpath.(scope / "PROTOCOL.md") in
+    let scope = B0_build.scope_dir b in
+    let md = Fpath.(scope // protocol_md) in
     let md_html = Fpath.(scope / "doc" / "md.html") in
     let md_css = Fpath.(scope / "doc" / "md.css") in
-    let build = B0_build.current_build_dir b in
-    let html = Fpath.(build / "PROTOCOL.html") in
-    let hfrag = Fpath.(build / "PROTOCOL.hfrag") in
-    List.iter (B0_memo.file_ready m) [md; md_html; md_css];
+    let build = B0_build.current_dir b in
+    let html = Fpath.(build // html) in
+    let hfrag = Fpath.(html -+ ".hfrag") in
+    B0_memo.ready_files m [md; md_html; md_css];
     B0_cmark.cmd m ~opts:Cmd.(arg "--unsafe") ~mds:[md] ~o:hfrag;
     begin B0_memo.write m ~reads:[hfrag; md_html; md_css] html @@ fun () ->
       Fut.sync @@
@@ -115,10 +104,7 @@ let protocol =
     end;
     Fut.return ()
   in
-  let meta =
-    B0_meta.empty
-    |> B0_meta.add B0_unit.exec ("show-uri", show_uri_action "PROTOCOL.html")
-  in
+  let meta = B0_meta.empty |> ~~ B0_show_url.url (`In (`Unit_dir, html)) in
   B0_unit.make "protocol" ~doc:"Description of the protocol" ~meta exe_proc
 
 let cell_gui =
@@ -128,41 +114,43 @@ let cell_gui =
     [fmt; gg; gg_kit; vg; vg_pdf; vg_htmlc; brr; note; note_brr; evidence]
   in
   let opts =
-    (* XXX I think should be no longer needed with jsoo 5. *)
+    (* TODO I think should be no longer needed with jsoo 5. *)
     Cmd.(arg "--enable=use-js-string")
   in
   let meta =
     B0_meta.empty
-    |> B0_meta.add B0_jsoo.compilation_mode `Whole
-    |> B0_meta.add B0_jsoo.compile_opts opts
-    |> B0_meta.add B0_jsoo.link_opts opts
-    |> B0_meta.add B0_jsoo.source_map None (* Some `Inline) *)
-    |> B0_meta.add B0_unit.exec ("show-uri", show_uri_action "cell.html")
+    |> ~~ B0_jsoo.compilation_mode `Whole
+    |> ~~ B0_jsoo.compile_opts opts
+    |> ~~ B0_jsoo.link_opts opts
+    |> ~~ B0_jsoo.source_map None (* Some `Inline) *)
+    |> ~~ B0_show_url.url (`In (`Unit_dir, ~/"cell.html"))
   in
   let wrap = build_cell_gui in
-  B0_jsoo.exe "gui.js" ~name:"cell_gui" ~requires ~meta ~srcs ~wrap ~doc
+  B0_jsoo.exe "gui.js" ~name:"cell-gui" ~requires ~meta ~srcs ~wrap ~doc
 
 let cell_exe =
   let srcs = [`Dir ~/"src"; `Dir_rec ~/"src/cli";] in
   let requires = [b0_std; fmt; gg; gg_kit; vg; vg_pdf; cmdliner; xmlm] in
   let meta =
-    (* FIXME b0: don't let jsoo builds downgrade everything to bytecode *)
+    (* TODO b0: don't let jsoo builds downgrade everything to bytecode *)
     B0_meta.empty
     |> B0_meta.add B0_ocaml.Code.needs `Native
   in
   B0_ocaml.exe "cell" ~meta ~srcs ~requires
 
+(* Actions *)
+
 let mount_dir = Fpath.v "nosync/deploy"
 let webdav_url = "https://cloud.uni-konstanz.de/public.php/webdav"
 let mount =
-  B0_action.make "mount" ~doc:"(Un)mount deploy directory" @@
-  fun action env ~args ->
+  B0_unit.of_action' "mount" ~doc:"(Un)mount deploy directory" @@
+  B0_unit.Action.of_cmdliner_term @@ fun env u ->
   let doc = "Unmount deploy directory." in
   let unmount = Cmdliner.Arg.(value & flag & info ["u"; "unmount"] ~doc) in
   let mount u d = Os.Cmd.run Cmd.(arg "mount_webdav" % "-i" % u %% path d) in
   let umount d = Os.Cmd.run Cmd.(arg "umount" %% path d) in
   let mount unmount =
-    B0_action.exit_of_result @@
+    Os.Exit.of_result @@
     let dir = Fpath.(B0_env.scope_dir env // mount_dir) in
     match unmount with
     | true ->
@@ -177,20 +165,19 @@ let mount =
         let* is_mount = Os.Path.is_mount_point dir in
         if is_mount then Ok () else mount webdav_url dir
   in
-  let term = Cmdliner.Term.(const mount $ unmount) in
-  B0_action.eval_cmdliner_term action env term ~args
+  Cmdliner.Term.(const mount $ unmount)
 
 let deploy =
   let doc = "Build and deploy to mount directory" in
-  B0_action.make' "deploy" ~units:[cell_gui; protocol] ~doc @@
-  fun _ env ~args ->
+  B0_unit.of_action "deploy" ~units:[cell_gui; protocol] ~doc @@
+  fun env _ ~args ->
   let dir = Fpath.(B0_env.scope_dir env // mount_dir) in
   let mount_error = Error "No deploy directory use 'b0 -- mount' first" in
   let* exists = Os.Dir.exists dir in
   if not exists then mount_error else
   let* is_mount = Os.Path.is_mount_point dir in
   if not is_mount then mount_error else
-  let protocol_md = B0_env.in_scope_dir env ~/"PROTOCOL.md" in
+  let protocol_md = B0_env.in_scope_dir env protocol_md in
   let protocol_html = B0_env.in_unit_dir env protocol ~/"PROTOCOL.html" in
   let changes_md = B0_env.in_scope_dir env ~/"CHANGES.md" in
   let cell_html = B0_env.in_unit_dir env cell_gui ~/"cell.html" in
